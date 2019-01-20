e8bab70a3fd467d7b18858329836adcf48d59e71
dlgks0733 <dlgks0733@naver.com>
2019-01-20 18:43:55 +0900

    MVC2 Factory Pattern Commit

---
+++ ShareHouse/src/com/sh/controller/ActionFactory.java
@@ -0,0 +1,38 @@
+package com.sh.controller;
+
+import com.sh.action.Action;
+import com.sh.action.MainAction;
+
+/**
+ * request로 받아온 매개변수의 command의 값으로 각 기능을 수행할 액션을 분기하는 클래스 <br>
+ * action추가 시 최대한 기능별로 정렬될 수 있도록 한다. 수정이 잦은 class이므로 주의하여 편집!!
+ * 
+ * @author cho
+ *
+ */
+
+public class ActionFactory {
+	private static ActionFactory instance = new ActionFactory();
+
+	private ActionFactory() {
+		super();
+	}
+
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+
+	public Action getAction(String command) {
+		Action action = null;
+
+		System.out.println("ActionFactory : " + command);
+
+		if (command.equals("main")) {
+
+			action = new MainAction();
+
+		}
+		return action;
+
+	}
+}
---
+++ ShareHouse/src/com/sh/controller/MainServlet.java
@@ -0,0 +1,67 @@
+package com.sh.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sh.action.Action;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MainServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String command = request.getParameter("command");
+		System.out.println("MainServlet에서 요청을 받음을 확인 : " + command);
+
+		if (command == null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("main.jsp");
+			dispatcher.forward(request, response);
+
+		} else if (command.equals("loginForm")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+			dispatcher.forward(request, response);
+
+		} else {
+			ActionFactory af = ActionFactory.getInstance();
+			Action action = af.getAction(command);
+
+			if (action != null) {
+				action.execute(request, response);
+			}
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
