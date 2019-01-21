5e935384fa139417bb5b7d9f946990162759b47b
chang hyeon LEE <dydehd6208@naver.com>
2019-01-21 20:10:34 +0900

    login.jsp MemberDAO.java MemberDAO.class insert

--- ShareHouse/src/com/sh/dao/MemberDAO.java
+++ ShareHouse/src/com/sh/dao/MemberDAO.java
@@ -2,6 +2,7 @@ package com.sh.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.sh.vo.MemberVO;
@@ -49,5 +50,109 @@ public class MemberDAO extends DBManager {
 		}
 		
 	}
+	
+	
+	//아이디 중복체크
+	public int confirmID(String userid) {
+		int result = -1;
+		String sql = "select userid from SHAREHOUSE where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try{
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,  userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				result = 1;
+			} else {
+				result = -1;
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			try{
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close(); 
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	//사용자 인증시 사용하는 메소드
+	public int userCheck(String userid, String pwd) {
+		int result = -1;
+		String sql = "select pwd from SHAREHOUSE where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try{
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				if(rs.getString("pwd")!=null && rs.getString("pwd").equals(pwd)){
+					result = 1;
+				} else {
+					result = 0;
+				}
+			}else {
+					result = -1;
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			try{
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close(); 
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	
+	//아이디로 회원 정보 가져오는 메소드
+	public MemberVO getMember(String userid) {
+		MemberVO mVo = null;
+		String sql = "select * from TBL_MEMBER where userid = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try{
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				mVo = new MemberVO();
+				mVo.setMemberId(rs.getString("memberId"));
+				mVo.setMemberPwd(rs.getString("memberPwd"));
+				mVo.setMemberName(rs.getString("memberName"));
+				
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close(); 
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return mVo;
+	}
+	
 	
 }
