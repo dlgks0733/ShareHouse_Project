0cf532d372bed00eab63442765491072c3b7dbad
dlgks0733 <dlgks0733@naver.com>
2019-01-21 15:25:56 +0900

    SNS, STARTUP, EPL BOARD FORM ACTION COMMIT

---
+++ ShareHouse/src/com/sh/controller/action/sns/BoardFormAction.java
@@ -0,0 +1,19 @@
+package com.sh.controller.action.sns;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sh.controller.action.Action;
+
+public class BoardFormAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
